d3cf867932d9b71cae90f554416c55452177c31c
MohamadAli20 <mohamadali.calanda@cvsu.edu.ph>
2024-06-01 03:45:11 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 
-// const config = {
-//     host: 'nodedatabase.cpeies8qgyq4.ap-southeast-1.rds.amazonaws.com',
-//     user: 'admin',
-//     password: 'testing123',   
-//     database: 'firedata_db'
-// };
+const config = {
+    host: 'nodedatabase.cpeies8qgyq4.ap-southeast-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'testing123',   
+    database: 'firedata_db'
+};
 
 // local testing
-const config = {
-    host: 'localhost',
-    user: 'root',
-    password: '',   
-    database: 'ph_fire_data'
-};
+// const config = {
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',   
+//     database: 'ph_fire_data'
+// };
     
 
 module.exports = config;
